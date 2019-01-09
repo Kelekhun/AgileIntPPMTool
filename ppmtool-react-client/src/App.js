b2ff708075df80ba3273d8dc2c196ced453b121c
Kelekhun <khadkadeepak@gmail.com>
2019-01-09 17:36:14 -0500

    Router Route Added

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,37 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Header />
-        <Dashboard />
-
-        {/*   
-      <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welecome to PPMtool</h1>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        */}
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
     );
   }
 }
-
 export default App;
