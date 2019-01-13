5ed5f2758dc38cf6cb40b6354dd134a761c357c6
Kelekhun <khadkadeepak@gmail.com>
2019-01-13 18:58:37 -0500

    Backlog & ProjectTast Added

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/domain/Project.java
@@ -38,6 +38,9 @@ public class Project {
 	@JsonFormat(pattern = "yyyy-mm-dd")
 	private Date updated_At;
 
+	@OneToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "project")
+	private Backlog backlog;
+
 	public Project() {
 	}
 
@@ -115,4 +118,11 @@ public class Project {
 		this.updated_At = new Date();
 	}
 
+	public Backlog getBacklog() {
+		return backlog;
+	}
+
+	public void setBacklog(Backlog backlog) {
+		this.backlog = backlog;
+	}
 }
