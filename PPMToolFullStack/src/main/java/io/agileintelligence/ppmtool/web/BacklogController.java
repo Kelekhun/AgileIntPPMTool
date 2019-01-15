d72ea1712d8185697a8c865aa5238030e3e375a3
Kelekhun <khadkadeepak@gmail.com>
2019-01-14 19:45:20 -0500

    ProjectTask Exception Handling

--- PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/BacklogController.java
+++ PPMToolFullStack/src/main/java/io/agileintelligence/ppmtool/web/BacklogController.java
@@ -30,9 +30,9 @@ public class BacklogController {
         //show delete
         //custom exception
 
-        ResponseEntity<?> erroMap = mapValidationErrorService.MapValidationService(result);
-        if (erroMap != null) return erroMap;
-
+    	ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if (errorMap != null) return errorMap;
+        
         ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
 
         return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
